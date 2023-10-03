50671902f97a3ea56ccf18de8289945068e37267
Kainak <natanfkainak@gmail.com>
2023-10-03 00:02:25 -0300

    .

--- projeto/schemas/index.js
+++ projeto/schemas/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+
+import noticia from './noticia.js'
+
+export const schemaTypes = [noticia]
+
---
+++ projeto/schemas/noticia.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'noticia',
+    type: 'document',
+    title: 'Noticia',
+    fields: [
+      {
+        name: 'titulo',
+        type: 'string',
+        title: 'Titulo'
+      },
+      {
+        name: 'subtitulo',
+        type: 'string',
+        title: 'Sub título'
+      },
+      {
+        name: 'conteudo',
+        type: 'string',
+        title: 'Conteúdo'
+      }
+    ]
+  }
