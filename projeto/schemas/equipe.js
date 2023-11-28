f7c87b0223ab061ee7ed16bfc051161f104d7bda
Vinicius Eduardo Agnes <114628323+ViniciusAgnes@users.noreply.github.com>
2023-11-27 21:26:47 -0300

    Update equipe.js

--- projeto/schemas/equipe.js
+++ projeto/schemas/equipe.js
@@ -7,17 +7,22 @@ export default {
       {
         name: 'nome',
         type: 'string',
-        title: 'Nome'
+        title: 'Nome',
+        validation: Rule => Rule.max(50).warning('A resposta deve ter no máximo 45 caracteres')
+
       },
       {
         name: 'cargo',
         type: 'string',
-        title: 'Cargo'
+        title: 'Cargo',
+        validation: Rule => Rule.max(45).warning('A resposta deve ter no máximo 40 caracteres')
+
       },
       {
         name: 'imagem',
         type: 'image',
         title: 'Imagem'
+
       }
     ]
   }
